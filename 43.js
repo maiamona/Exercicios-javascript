3e0f092cba404422d0d0cac5370120a5b3a2cf73
Maiamona Garcia <maiamona.garcia@jupiter.co.ao>
2021-12-28 08:53:00 +0100

    debugger

---
+++ 43.js
@@ -0,0 +1,16 @@
+// 2. Usando o depurador
+// A debuggerpalavra-chave interrompe a execução do código e chama a função de depuração.
+
+// O debuggerestá disponível em quase todos os mecanismos JavaScript.
+
+// Vamos ver um exemplo,
+
+
+let a = 6;
+let b = 9;
+let c = a * b;
+
+// stops the execution
+debugger;
+
+console.log(c);
